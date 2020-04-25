f3dd8320b1c285acff6b46196dda1c7b6bc21697
AzizTurki356 <62173783+AzizTurki356@users.noreply.github.com>
2020-04-25 15:49:52 +0100

    Create checkPointfinal.cs

---
+++ checkPointfinal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+
+    float Sum(float a, float b){
+	return a + b;
+}
+    Int Prod( int a, int b)}
+    return a*b; 
+}
+    Div check0 ( float a , float b)}
+    float z=a*b; 
+    return z>0;
+}
